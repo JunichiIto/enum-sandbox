065d12802bce0fdf615e4a5425ae4c2ab00d02fc
Junichi Ito <jit@sonicgarden.jp>
2017-09-27 08:39:04 +0900

    Refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,4 @@ Rails.application.routes.draw do
   resources :articles do
     patch :toggle_status
   end
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/system/articles_test.rb
+++ test/system/articles_test.rb
@@ -4,8 +4,10 @@ class ArticlesTest < ApplicationSystemTestCase
   test 'toggle status' do
     visit article_path(articles(:one))
     assert_link 'published'
+
     click_link 'published'
     assert_link 'draft'
+
     click_link 'draft'
     assert_link 'published'
   end
