194d54b60b1c5f978c211f873d1ff84a3c4323c3
Junichi Ito <jit@sonicgarden.jp>
2017-09-27 07:53:13 +0900

    Add link to toggle status

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,12 @@ class Article < ApplicationRecord
   enum status: { draft: 0, published: 1 }
   validates :title, :content, :status, presence: true
   validates :status, inclusion: { in: Article.statuses.keys }
+
+  def toggle_status!
+    if draft?
+      published!
+    else
+      draft!
+    end
+  end
 end
