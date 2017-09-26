80853e8c65283cd740f125ec95acb7168e7fc775
Junichi Ito <jit@sonicgarden.jp>
2017-09-27 07:58:46 +0900

    Add test for toggle status

--- test/system/articles_test.rb
+++ test/system/articles_test.rb
@@ -1,9 +1,12 @@
 require "application_system_test_case"
 
 class ArticlesTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit articles_url
-  #
-  #   assert_selector "h1", text: "Article"
-  # end
+  test 'toggle status' do
+    visit article_path(articles(:one))
+    assert_link 'published'
+    click_link 'published'
+    assert_link 'draft'
+    click_link 'draft'
+    assert_link 'published'
+  end
 end
