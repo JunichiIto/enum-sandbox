d758ee84d594e8339f1b5ff9cff0c55b7eb6b3bc
yameguun <nsy999jp@gmail.com>
2017-09-27 22:08:22 +0900

    schemaをmodelに書き出した

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: articles
+#
+#  id         :integer          not null, primary key
+#  title      :string           not null
+#  content    :text             not null
+#  status     :integer          default("draft"), not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Article < ApplicationRecord
   enum status: { draft: 0, published: 1 }
   validates :title, :content, :status, presence: true
